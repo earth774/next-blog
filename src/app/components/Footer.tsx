aa0246f05da8375c4be5a2113c9f5601eb9f3205
earth774 <sutthiphongnuanma@gmail.com>
2025-02-22 14:18:40 +0700

    feature: update new blog about กำหนด primary key เป็นเลขล่าสุดทุก table

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,15 @@
-
 const Footer = () => {
-    return (
-        <footer className="mt-auto mb-4 text-center text-sm text-black">
-            <em>@ amiearth</em>
-        </footer>
-    )
-}
+  return (
+    <footer className="mt-auto mb-4 text-center text-sm text-black">
+      <a
+        href="https://x.com/SutthiponGEarth"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <em>@ amiearth</em>
+      </a>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
