37c8174a1310c00c13d8b7a2dd3e1c66e55e6b7a
earth774 <sutthiphongnuanma@gmail.com>
2025-08-09 12:46:46 +0700

    chore: add @next/third-parties dependency and integrate Google Analytics into layout for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -69,6 +70,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         ></script>
       </body>
+      <GoogleAnalytics gaId="G-JRKV3SB66K" />
     </html>
   );
 }
